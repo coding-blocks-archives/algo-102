01a1c2225775e296e15202905790f3882e369908
prateek27 <prateeknarang111@gmail.com>
2017-12-15 14:32:53 +0530

    added codes?

---
+++ Lecture-4/india.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+int main()
+{
+    int a, b, c;
+    for (b=c=10; a="Hello!Welcome to GeeksForGeeks.\
+      TFy!QJu ROo TNn(ROo)SLq SLq ULo+\
+      UHs UJq TNn*RPn/QPbEWS_JSWQAIJO^\
+      NBELPeHBFHT}TnALVlBLOFAkHFOuFETp\
+      HCStHAUFAgcEAelclcn^r^r\\tZvYxXy\
+      T|S~Pn SPm SOn TNn ULo0ULo#ULo-W\
+      Hq!WFs XDt!" [b+++21]; )
+
+      for (; a-- > 64 ; )
+         putchar ( ++c=='Z' ? c = c/ 9:33^b&1);
+    return 0;
+}
